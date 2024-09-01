3ea69bbe0d93adbcfb3be5f6cb3ae2ae05b4962b
Mudassar-Rasool09 <eshajabeen886@gmail.com>
2024-09-01 17:56:27 +0500

    uodated

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,18 @@ const About = lazy(() => import('./Pages/About'));
 function App() {
   const darkMode = useSelector((state) => state.theme.darkMode);
 
+  // Select all elements with the class 'cursor'
+var cursors = document.getElementsByClassName('cursor');
+
+
+// Add event listener for mouse movement to move the cursor
+window.addEventListener('mousemove', (dets) => {
+    for (let i = 0; i < cursors.length; i++) {
+        cursors[i].style.left = dets.clientX + 'px';
+        cursors[i].style.top = dets.clientY + 'px';
+    }
+});
+
   return (
     <>
       <div className={`cursor ${darkMode ? 'bg-white shadow-lg' : 'bg-gray-800'}`}></div>
